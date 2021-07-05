24a3da840a85e61f07aa87b62b4e1f2b02032794
Fabricio-Barreto <63824002+Fabricio-Barreto@users.noreply.github.com>
2021-07-05 15:18:19 -0300

    Iniciando Backend

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,10 +2,28 @@ import  Modal from './modal.js'
 
 const modal = Modal()
 
+const modalTitle = document.querySelector('.modal h2')
+const modalDescription = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal button')
+
 const checkButtons = document.querySelectorAll(".actions a.check")
 
 checkButtons.forEach(button => {
     button.addEventListener("click", event => {
+
+        event.preventDefault()
+
+        const roomId = document.querySelector("#room-id").dataset.id
+        const form = document.querySelector(".modal form")
+        const questionId = event.target.dataset.id
+
+        form.setAttribute("action", `/room/${roomId}/${questionId}/check`)
+
+        modalTitle.innerHTML = "Marcar como lida"
+        modalDescription.innerHTML = "Tem certeza que desja marcar como lida esta pergunta?"
+        modalButton.innerHTML = "Sim, Marcar"
+        modalButton.classList.remove("red")
+
         modal.open()
     })
 })
@@ -14,6 +32,20 @@ const deleteButton = document.querySelectorAll(".actions a.delete")
 
 deleteButton.forEach(button => {
     button.addEventListener("click", event =>{
+
+        event.preventDefault()
+
+        const roomId = document.querySelector("#room-id").dataset.id
+        const form = document.querySelector(".modal form")
+        const questionId = event.target.dataset.id
+
+        form.setAttribute("action", `/room/${roomId}/${questionId}/delete`)
+
+        modalTitle.innerHTML = "Excluir pergunta"
+        modalDescription.innerHTML = "Tem Certeza que você deseja Excluir esta pergunta ?"
+        modalButton.innerHTML = "Sim, Excluir"
+        modalButton.classList.add("red")
+
         modal.open()
     })
 })
--- src/route.js
+++ src/route.js
@@ -1,10 +1,14 @@
 const express = require('express')
+const questionController = require('./controllers/questionController')
 
 const route = express.Router()
 
 route.get('/', (req, res) => res.render("index"))
 route.get('/room', (req, res) => res.render("room"))
 route.get('/create-pass', (req, res) => res.render("create-pass"))
+
+route.post('/room/:room/:question/:action', questionController.index)
+
 
 
 module.exports = route
